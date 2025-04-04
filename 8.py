5f862497b45faa3b22fbf9b75e0b3e9fe792d8b8
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-04 18:49:23 +0530

    Done arithmetic operators

---
+++ 8.py
@@ -0,0 +1,17 @@
+# Arithmetic operators
+# Arithmetic operators are used to perform mathematical operations on numbers.
+# Python has the following arithmetic operators:
+# +  Addition
+# -  Subtraction
+# *  Multiplication
+# /  Division
+# %  Modulus (Remainder)
+# ** Exponentiation
+# // Floor division (quotient)
+
+# print(5 + 2)
+# print(5 - 2)
+# print(5 * 2)
+# print(5 / 2)
+# print(5 % 2)
+# print(5 ** 2)
