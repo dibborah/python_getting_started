0aae02b233812754c75487c593c5741aa066dac9
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-04 16:34:33 +0530

    Strings done

---
+++ 5.py
@@ -0,0 +1,27 @@
+# Strings
+
+course = 'Python"s for Begineers'
+# course = '''
+# Hi John, 
+# Here is the first email to you,
+
+# Thank you for your support!!!
+# '''
+
+# print(course)
+# Python supports negative indexing
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:]) # Same like slice or substring
+# print(course[1:])
+# print(course[:5])
+# print(course[:])
+
+# another_course = course[:]
+
+# print(course)
+# print(another_course)
+
+name = 'Jennifer'
+
+print(name[1:-1])
