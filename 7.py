8f0f03220d7e43a6f0f9a989cc7037ba72f2c54b
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-04 17:25:49 +0530

    String methods: Done

---
+++ 7.py
@@ -0,0 +1,17 @@
+# Strings methods
+
+course = "Python for Beginners"
+# print(len(course)) # length of the string(len())
+# print(course.upper()) # convert to upper case
+# print(course.lower()) # convert to lower case
+# print(course.title()) # convert to title case(first letter of each word to upper case)
+# print(course.strip()) # remove leading and trailing spaces
+# print(course.find("P")) # find the index of the first occurrence of "P"
+# print(course.replace("P", "J")) # replace "P" with "J"
+# print(course.replace("Python", "Java")) # replace "Python" with "Java"
+# print("Python" in course) # check if "Python" is in the string
+# print("Python" not in course) # check if "Python" is not in the string
+# print(course.split(" ")) # split the string into a list of words
+print(course.split("o")) # split the string into a list of words using "o" as the delimiter
+print(course.split(" ")) # split the string into a list of words using space as the delimiter
+print(course.split(".")) # split the string into a list of words using "," as the delimiter
