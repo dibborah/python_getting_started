ff46b0d79ec5597a3bf30a180b2a53aaef5fa070
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-04 18:58:26 +0530

    Done math module functions overview

---
+++ 10.py
@@ -0,0 +1,11 @@
+# Math Function
+
+import math
+
+print(math.ceil(2.1))
+print(math.floor(2.1))
+
+# round() method is not part of the math imported module
+x = 2.9
+print(round(x))
+# print(abs(-2.9))
