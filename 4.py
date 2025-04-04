ca5272674dca9cfa8b86f555e4c2122f013113e5
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-04 16:23:42 +0530

    Type conversion and some extra file

---
+++ 4.py
@@ -0,0 +1,32 @@
+# Type conversions
+
+# birth_year = input('Birth year: ')
+# age = 2019 - birth_year # Type error
+# age = 2019 - int(birth_year)
+# print(age)
+# print(int(birth_year))
+
+# Type conversion
+# int() # string to integer
+# float() # string to decimal point
+# bool() # string to boolean
+# str() # integer to string
+# list() # string to list
+# tuple() # string to tuple
+# dict() # string to dictionary
+# set() # string to set
+# complex() # string to complex number
+# eval() # string to expression
+
+# print(type(birth_year))
+# print(type(age))
+
+
+# Exercise
+weight_lbs = input('What is your weight in pounds? ')
+weight_in_kg = int(weight_lbs) * 0.45
+
+print('Your weight in kg is ' + str(weight_in_kg))
+
+
+
