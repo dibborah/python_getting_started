ee88e17775c1704d7ca88ffbcdf3b72609af654e
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 12:57:51 +0530

    Done lists methods

---
+++ 22.py
@@ -0,0 +1,46 @@
+# Lists methods 
+
+numbers = [2, 1, 5, 7, 5, 4, 5]
+
+# append()
+# insert()
+# index()
+# pop()
+# remove()
+# clear()
+# count()
+# sort()
+# reverse()
+# copy()
+
+# numbers.append(100)
+# print(numbers)
+
+# numbers.insert(1, 600)
+# print(numbers)
+
+# print(numbers.index(5))
+
+# print(numbers.pop())
+# print(numbers)
+
+# print(numbers.remove(5)) # Doesnot return anything( None will be printed)
+# print(numbers)
+
+# print(numbers.clear()) # Doesnot return anything( None will be printed)
+# print(numbers)
+
+# print(numbers.count(5))
+
+# print(numbers.sort()) # Doesnot return anything( None will be printed)
+# print(numbers)
+
+# print(numbers.reverse()) # Doesnot return anything( None will be printed)
+# print(numbers)
+
+# print(numbers)
+# numbers2 = numbers.copy()
+# numbers.sort()
+
+# print(numbers)
+# print(numbers2)
