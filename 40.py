32d2415619237867cea6d0545b889314d99bef89
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 20:55:39 +0530

    Done: Task of  Generating random tuple values

--- 40.py
+++ 40.py
@@ -5,6 +5,25 @@ import random
 #     # print(random.random())
 #     print(random.randint(10, 20))
 
-members = ['John', 'Mary', 'Bob', 'Mosh']
-print(random.choice(members))
+# members = ['John', 'Mary', 'Bob', 'Mosh']
+# print(random.choice(members))
 
+# Task:
+
+# Roll & dice
+
+# Dice {class}
+#   - roll() {tuple}
+
+
+class Dice:
+    def roll(self):
+        # random_nums = (random.randint(1, 6), random.randint(7,12))
+        # return random_nums
+        first = random.randint(1, 6)
+        second = random.randint(7,12)
+        return first, second
+
+
+dice = Dice()  
+print(dice.roll())
