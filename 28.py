c7960dc83f8a0a7b8c2d252652698f98af971272
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 14:57:21 +0530

    Done parameters

---
+++ 28.py
@@ -0,0 +1,21 @@
+# Parameters
+
+
+# def greet_user(name):
+#     print(f'Hi {name}!')
+#     print('Welcome abroad!')
+
+
+# print('Start')
+# greet_user('Elon')
+# greet_user()
+# print('Finish')
+
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name} {last_name}!')
+    print('Welcome abroad!')
+
+
+print('Start')
+greet_user('Elon', 'Musk')
+print('Finish')
