40d5ba0c995e1100e4b3b556e8e8192d09977c9a
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 13:58:27 +0530

    Done functions

---
+++ 27.py
@@ -0,0 +1,14 @@
+# Functions
+
+# Smaller maintainable and managable chunks: Functions
+
+# def: reserve keyword in python for defining a keyword: Python interpreter immediately recognizes this
+
+def greet_user():
+    print('Hii')
+    print('Welcome abroad')
+
+
+print('Start')
+greet_user()
+print('Finish')
