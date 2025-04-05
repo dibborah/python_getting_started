c44cb1384e66b4cb94e2e557ee4680018571477c
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 13:19:24 +0530

    Done unpacking

---
+++ 24.py
@@ -0,0 +1,12 @@
+# unpacking is similar to destructuring
+
+coordinates = (1,2,3)
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+x, y, z = coordinates
+
+# print(f'{x},{y},{z}')
+# print(x, y, z)
+print(y)
