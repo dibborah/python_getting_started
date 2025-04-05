a0e18f5fb346800b6f51b5643f1cd2a671a4f067
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 13:28:23 +0530

    Done dictionaries

---
+++ 25.py
@@ -0,0 +1,13 @@
+# Dictionaires
+
+customer = {
+    "name": 'John smith',
+    'age': '30',
+    'is_verified': True,
+    5: 'Five',
+}
+
+customer['name'] = 'Elon musk'
+customer['dob'] = 'feb 14 1931'
+# print(customer.get('dob', '12 jan 1981'))
+print(customer)
