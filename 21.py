bf80e762c6b313b7a6411be6775276f846f155ee
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 12:42:07 +0530

    Done 2D lists

---
+++ 21.py
@@ -0,0 +1,18 @@
+# 2D lists
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]
+
+# print(matrix)
+# matrix [0] [1] = 500
+# print(matrix)
+
+# for row in matrix:
+#     print(row)
+
+for row in matrix:
+    for i in row: 
+        print(i)
