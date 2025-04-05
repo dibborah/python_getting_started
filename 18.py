c14aaa6f42f47ea90131f94d0c9523251a8d6d5c
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 12:17:50 +0530

    Done for loops

---
+++ 18.py
@@ -0,0 +1,26 @@
+# loops
+
+# iterating a string
+# for item in 'python':
+#     print(item)
+
+# iterating a list
+# for item in ['python', 'is', 'a', 'powerful', 'language']:
+#     print(item)
+
+# for i in [1,2,3,4]:
+#   print(i)
+
+# Range funtion help us to iterate over a large number w/o us explicitly defining all the number till the large no.
+# Adding range
+# for i in range(5, 10, 2):
+    # print(i)
+
+# task
+prices = [10, 20, 30]
+# calculate total cost
+
+total = 0
+for price in prices:
+    total += price
+print(f'total: {total}')
