ecb27fe0f0d54f44de221af9d44eb0cb85ed87c6
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 13:37:48 +0530

    Done dictionaries Task

--- 25.py
+++ 25.py
@@ -1,4 +1,4 @@
-# Dictionaires
+# Dictionaries
 
 customer = {
     "name": 'John smith',
@@ -10,4 +10,27 @@ customer = {
 customer['name'] = 'Elon musk'
 customer['dob'] = 'feb 14 1931'
 # print(customer.get('dob', '12 jan 1981'))
-print(customer)
+# print(customer)
+
+# Task:
+
+phoneNumbers = input('Phone: ')
+
+phone = {
+    '0': 'Zero',
+    '1': 'One',
+    '2': 'Two',
+    '3': 'Three',
+    '4': 'Four',
+    '5': 'Five',
+    '6': 'Six',
+    '7': 'Seven',
+    '8': 'Eight',
+    '9': 'Nine',
+}
+
+output = ''
+for number in phoneNumbers:
+    output += phone.get(number, '!') + ' '
+
+print(output.strip())
