7c9798d80f850eb70188dfb1e2ab1c21d2faecf1
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 16:33:15 +0530

    Done: module(import)

---
+++ 37.py
@@ -0,0 +1,11 @@
+# modules
+
+import converters
+from converters import lbs_to_kgs
+from converters import kgs_to_lbs
+
+print(converters.lbs_to_kgs(100))
+print(converters.kgs_to_lbs(45))
+
+print(lbs_to_kgs(100))
+print(kgs_to_lbs(45))
---
+++ converters.py
@@ -0,0 +1,5 @@
+def lbs_to_kgs(lbs):
+    return lbs * 0.45
+
+def kgs_to_lbs(kgs):
+    return kgs / 0.45
