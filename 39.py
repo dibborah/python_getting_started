482333772bfb13de9b7450f9f188bad6a163d09b
dibya.borah <dibyajyotiborah469@gmail.com>
2025-04-05 17:00:25 +0530

    Done: packages

---
+++ 39.py
@@ -0,0 +1,12 @@
+# packages
+
+# A package is container of related modules
+
+import ecommerce.shipping
+from ecommerce.shipping import calc_shipping
+from ecommerce import shipping
+# from ecommerce.shipping import calc_shipping, calc_tax, etc...
+
+# ecommerce.shipping.calc_shipping()
+# calc_shipping()
+shipping.calc_shipping()
